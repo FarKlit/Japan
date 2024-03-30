a0ccb353193e9297e4621ae487dd934bd2fff661
FarKlit <farruhk91@gmail.cocm>
2024-03-30 16:13:08 +0500

    loader

--- js/app.js
+++ js/app.js
@@ -10,3 +10,32 @@
         }
     }
 })();
+
+(function() {
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const close = document.querySelector('.header__nav-close');
+    burger.addEventListener('click', () => {
+        menu.classList.add('header__nav-active');
+    });
+    close.addEventListener('click', () => {
+        menu.classList.remove('header__nav-active');
+    });
+}) ();
+
+
+
+// setTimeout(function () {
+//     const mask = document.querySelector('.mask');
+//     mask.classList.add('none');
+// }, 1000);
+
+(function() {
+    const mask = document.querySelector('.mask')
+    window.addEventListener('load', () => {
+        mask.classList.add('hide')
+        setTimeout(() => {
+            mask.remove()
+        }, 1000)
+    })
+})();
